eec89dba42a5f3c76a4beec27aa37401864ea3d0
Jared Lam <46579874+EngineeringForever2018@users.noreply.github.com>
2022-05-31 17:45:18 -0700

    Create Packet.cpp

---
+++ Packet.cpp
@@ -0,0 +1,39 @@
+#include "Packet.h"
+#include <string>
+
+Packet::Packet(string header)
+{
+    this -> setHeader(header);
+}
+
+Packet::Packet() {}
+
+void Packet::setHeader(string header)
+{
+    this -> Header = header;
+}
+
+string Packet::getHeader()
+{
+    return this -> Header;
+}
+
+void Packet::setDestination(int dest) 
+{
+  this->destination = dest;
+}
+
+int Packet::getDestination() 
+{
+   return destination;
+}
+
+void Packet::setSource(int src) 
+{
+   this->source = src;
+}
+        
+int Packet::getSource() 
+{
+  return source;
+}
